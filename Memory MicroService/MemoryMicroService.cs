5a439495e79a73fee63309ccf68f6de86dfbf484
C K Siew <ck_siew@hotmail.com>
2018-07-16 18:34:36 +0800

    Update MemoryMicroService

--- Memory MicroService/MemoryMicroService.cs
+++ Memory MicroService/MemoryMicroService.cs
@@ -2,6 +2,7 @@
 using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime;
 using System.Text;
@@ -37,7 +38,28 @@ namespace Memory_MicroService
             int gen2 = 0;
             for (int x=0; x < GC.MaxGeneration; x++)
             {
-
+                if (x == 0)
+                    gen1 = GC.CollectionCount(x);
+                else if (x == 1)
+                    gen2 = GC.CollectionCount(x);
+                Console.WriteLine("ttGeneration " + (x) + " Collection Count: " + GC.CollectionCount(x));
+                const string category = ".NET CLR Memory";
+                const string counter = "% Time in GC";
+                string instance = Process.GetCurrentProcess().ProcessName;
+                float percent = 0.0F;
+                if (PerformanceCounterCategory.Exists(category) && PerformanceCounterCategory.CounterExists(counter,category)&&
+                    PerformanceCounterCategory.InstanceExists(instance, category))
+                {
+                    var gcPerf = new PerformanceCounter(category, counter, instance);
+                    percent = gcPerf.NextValue();
+                    string suffix = "%";
+                    if (percent > 50.0)
+                    {
+                        suffix += " <- High Watermark Warning";
+                    }
+                    Console.WriteLine("ttTime Spent in GC: " + $"{percent:00.##}" + suffix);
+                }
+                Subscribe();
             }
 
         }
